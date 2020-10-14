d5850378298c7a1edba809b046c60b28724831f5
Bereket Girma <bgirma@Berekets-MacBook-Pro.local>
2020-10-14 09:01:34 -0700

    more update

--- client/src/features/Invite/Invite.js
+++ client/src/features/Invite/Invite.js
@@ -9,18 +9,21 @@ import {
   addGoing,
   addNotGoing,
 } from "./inviteSlice";
-import { Link } from "react-dom";
-import './invite.css'
+import { Link } from "react-router-dom";
+import "./invite.css";
 
 export function Invite() {
   const user = useSelector(selectInvite);
   const dispatch = useDispatch();
-  const notGoingUser = useSelector(selectNotGoing);
 
-  const goingUser = useSelector(selectGoing);
+  //   const notGoingUser = useSelector(selectNotGoing);
 
-//   const [randomUser] = useState();
-//   const [going, setGoing] = useState(0);
+  //   const goingUser = useSelector(selectGoing);
+
+  //   const [randomUser] = useState();
+  const [going, setGoing] = useState(0);
+  const [Notgoing, setNotGoing] = useState(0);
+
   useEffect(() => {
     dispatch(getInvite());
   }, []);
@@ -28,58 +31,54 @@ export function Invite() {
   function handleNotGoing(e) {
     e.preventDefault();
     dispatch(addNotGoing(user));
+    setNotGoing(Notgoing + 1);
     // dispatch(getInvite(user));
   }
 
   function handleGoing(e) {
     e.preventDefault();
     dispatch(addGoing(user));
-    //   setGoing(going + 1)
+    setGoing(going + 1);
   }
 
   return (
-      <div>
-
-
-    <div className="userContainer">
-    <div className="attending">
-        {/* <Link to="/api/going"> */}
-          <button className="btn going">Going: {goingUser.length}</button>
-        {/* </Link> */}
-        {/* <Link to="/api/notGoing"> */}
-          <button className="btn notGoing">Not going: {notGoingUser.length}</button>
-        {/* </Link> */}
-      </div>
-
-
-
-      <div className="userInfo">
-        <img className="user-image" src={user.picture} alt="userpic" />
-        <div> <hr></hr></div>
-        <div className="info">
-          <strong>Name:</strong> {user.first + " " + user.last} {user.last}
-        </div> 
-        
-
-        <div className="info">
-          <strong>Phone:</strong> {user.phone}
+    <div>
+      <div className="userContainer">
+        <div className="attending">
+          <Link to="/going">Going: {going}</Link>
+          <Link to="/notGoing">Not going: {Notgoing}</Link>
         </div>
 
-        <div className="info">
-          <strong>Email:</strong> {user.email}
+        <div className="userInfo">
+          <img className="user-image" src={user.picture} alt="userpic" />
+          <div>
+            {" "}
+            <hr></hr>
+          </div>
+          <div className="info">
+            <strong>Name:</strong> {user.first + " " + user.last} {user.last}
+          </div>
+
+          <div className="info">
+            <strong>Phone:</strong> {user.phone}
+          </div>
+
+          <div className="info">
+            <strong>Email:</strong> {user.email}
+          </div>
+        </div>
+        <div>
+          <hr></hr>
+        </div>
+        <div className="button-div">
+          <button onClick={handleNotGoing} className="x-button">
+            x
+          </button>
+          <button onClick={handleGoing} className="check-button">
+            &#10003;
+          </button>
         </div>
       </div>
-      <div> <hr></hr></div>
-      {/* button */}
-      <div className="button-div">
-        <button onClick={handleNotGoing} className="x-button">
-          x
-        </button>
-        <button onClick={handleGoing} className="check-button">
-          &#10003;
-        </button>
-      </div>
-    </div>
     </div>
   );
 }
